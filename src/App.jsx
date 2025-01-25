5f12225e2c5a8cc28924e34c51208764db4b70ef
Raj <rajbaidyanath76@gmail.com>
2025-01-26 03:13:06 +0530

    p

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,56 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Search from "./components/Search";
+
+const API_BASE_URL = 'https://api.themoviedb.org/3/discover/movie';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => {
 const [ searchTerm, setSearchTerm] = useState('');
+const [errorMessage, setErrorMessage] = useState('');
+const [movieList, setMovieList] = useState(initialState: []);
+const [isLoading, setIsLoading] = useState(initialState: false);
+
+const fetchMovies = async () => {
+  setIsLoading(value: true);
+  setErrorMessage(value: '');
+  try{
+    const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+    const response = await fetch(endpoint, API_OPTIONS);
+
+    if(!response.ok) {
+      throw new Error('Failed to fetch movies');
+    }    
+
+    const data = await response.json();
+
+    if(data.Response === 'False') {
+      setErrorMessage( value:data.Error || 'Failed to fetch Movies');
+      setMovieList(value: []);
+      return;
+    }
+
+    setMovieList(value:data.results || []);    
+  } catch(error){
+    console.log(`Error fetching movies: ${error}`);
+    setErrorMessage(value: 'Error fetching movies. Please try again later.');
+  }finally {
+     setIsLoading(value: false); 
+  }
+}
+
+ useEffect( effect: () => {
+  fetchMovies();
+ }, deps:[]);
 
   return (
     <main>
@@ -14,10 +62,15 @@ const [ searchTerm, setSearchTerm] = useState('');
           <h1>Find <span className="text-gradient">Movies</span>
           You will Enjoy Without the Hustle
           </h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
 
+        <section className='all-movies'>
+            <h2>All Movies</h2>
+        </section>
         
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        
       </div>
       
 
