2320e2dffa650a5c4b88637f86274703a0a5de99
Raj <rajbaidyanath76@gmail.com>
2025-03-21 23:38:19 +0530

    hj

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,13 @@
-const MovieCard = ({ movie: { title, vote_average, poster_path, release_date } }) => {
+// import React from 'react';
+import PropTypes from 'prop-types';
+
+const MovieCard = ({ movie }) => {
+    const { title, vote_average, poster_path, release_date } = movie || {};
+
+    // Format the rating and release date for fallback scenarios
+    const formattedVoteAverage = vote_average ? vote_average.toFixed(1) : 'N/A';
+    const formattedReleaseYear = release_date ? release_date.split('-')[0] : 'N/A';
+
     return (
       <div className="movie-card">
           {/* Ensure the poster image URL is valid */}
@@ -13,19 +22,30 @@ const MovieCard = ({ movie: { title, vote_average, poster_path, release_date } }
               <div className="content">
                   <div className="rating">
                       {/* If the star icon doesn't exist, provide a fallback */}
-                      <img src="star.png" alt="Star Icon" onError={(e) => e.target.src = '/default-star.png'} />
-                      <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+                      <img 
+                          src="star.png" 
+                          alt="Star Icon" 
+                          onError={(e) => e.target.src = '/default-star.png'} 
+                      />
+                      <p>{formattedVoteAverage}</p>
                   </div>
   
                   <span>•</span>
-                  <p className="year">
-                      {release_date ? release_date.split('-')[0] : 'N/A'}
-                  </p>
+                  <p className="year">{formattedReleaseYear}</p>
               </div>
           </div>
       </div>
     );
-  };
-  
-  export default MovieCard;
-  
+};
+
+// Adding PropTypes validation for the props
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string,
+        vote_average: PropTypes.number,
+        poster_path: PropTypes.string,
+        release_date: PropTypes.string
+    }).isRequired
+};
+
+export default MovieCard;
